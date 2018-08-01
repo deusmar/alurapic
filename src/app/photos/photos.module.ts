7a1277b5e40c4e54c3513d9dd65cdde083243e72
Deusmar Junior <deusmar.junior@hotmail.com>
2018-08-01 07:33:45 -0300

    Aula 04

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { PhotoComponent } from "./photo/photo.component";
-import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from "./photo-list/photo-list.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-    declarations: [PhotoComponent],
-    exports: [PhotoComponent],
+    declarations: [
+        PhotoComponent, 
+        PhotoListComponent
+    ],
     imports:[HttpClientModule]
 })
 export class PhotosModule {
